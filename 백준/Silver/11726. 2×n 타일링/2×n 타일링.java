d83b170bd8a51bbf16179c817bda96f338e7cdd9
Jinsoo Park <75584814+jinny908@users.noreply.github.com>
2024-09-24 01:27:43 +0900

    [Silver III] Title: 2×n 타일링, Time: 116 ms, Memory: 12856 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,21 @@
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        int[] dp = new int[1001];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++)
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+
+        System.out.println(dp[n]);
+
+    }
+}
+
