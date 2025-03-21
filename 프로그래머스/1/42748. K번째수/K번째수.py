430081617cfd787e8fb59f4eea103235d81baa59
Jinsoo Park <75584814+jinny908@users.noreply.github.com>
2025-03-21 21:48:37 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -2,8 +2,7 @@ def solution(array, commands):
     arr = []
     answer = []
     for cmd in commands:
-        arr = array[cmd[0]-1:cmd[1]]
-        arr.sort()
+        arr = sorted(array[cmd[0]-1:cmd[1]])
         answer.append(arr[cmd[2]-1])
     
     return answer
