27eca6dd1908ab92a7803a588702e722f9ecab9e
Jinsoo Park <75584814+jinny908@users.noreply.github.com>
2025-04-02 01:05:11 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 240 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,30 @@
+import sys
+
+# input = sys.stdin.readline
+
+while True:
+    word = input()
+    stack = []
+
+    if word =='.':
+        break
+
+    for char in word:
+        if char == '(' or char == '[':
+            stack.append(char)
+        elif char == ')':
+            if len(stack) != 0 and stack[-1] == '(':
+                stack.pop()
+            else:
+                stack.append(char)
+                break
+        elif char == ']':
+            if len(stack) != 0 and stack[-1] == '[':
+                stack.pop()
+            else:
+                stack.append(char)
+                break
+    if len(stack) == 0:
+        print('yes')
+    else:
+        print('no')
