0d6fb75dd50b034e6094cfd0dea7364533777fc7
Jinsoo Park <75584814+jinny908@users.noreply.github.com>
2025-04-04 01:37:30 +0900

    [level 3] Title: 징검다리 건너기, Time: 462.87 ms, Memory: 17.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -0,0 +1,26 @@
+def cross(stones,k,mid):
+    skip = 0
+    for stone in stones:
+        if stone - mid < 0:
+            skip += 1
+            if skip >= k:
+                return False
+        else:
+            skip = 0
+    return True
+
+def solution(stones, k):
+    left = 1
+    right = max(stones)
+    answer = 0
+    
+    
+    
+    while left <= right:
+        mid = (left + right) // 2
+        if cross(stones, k, mid):
+            answer = mid
+            left = mid + 1
+        else:
+            right = mid - 1
+    return answer
