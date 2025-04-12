5a451a559b4f82d67de993b6d931270c9154bd18
Jinsoo Park <75584814+jinny908@users.noreply.github.com>
2025-04-12 20:58:08 +0900

    [Gold III] Title: 나무 재테크, Time: 644 ms, Memory: 137904 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.py
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.py
@@ -1,75 +1,62 @@
+import sys
 from collections import deque
 
-n, m, k = map(int, input().split(' '))
+input = sys.stdin.readline
 
-# 양분 양 배열
-a = [list(map(int, input().split(' '))) for _ in range(n)]
-# 초기 밭 배열
-graph = [[5] * n for _ in range(n)]
-# 나무들 상태 저장할 배열
-trees = [[deque() for _ in range(n)] for _ in range(n)]
-# 위치별 죽은 나무들 저장할 배열
-dead_trees = [[list() for _ in range(n)] for _ in range(n)]
+n, m, k = map(int,input().split())
+feed = [list(map(int, input().split())) for _ in range(n)]
+tree = [list(map(int, input().split())) for _ in range(m)] # [x, y, age]
+land = [[5]*n for _ in range(n)]
+tree_map = [[deque() for _ in range(n)] for _ in range(n)]
+dead = [[[] for _ in range(n)] for _ in range(n)]
 
-# 입력받은 초기 나무 위치, 나이 저장
-for _ in range(m):
-    x, y, z = map(int, input().split(' '))
-    trees[x - 1][y - 1].append(z)
+for x, y, age in tree:
+    tree_map[x-1][y-1].append(age)
 
-dx = [-1, -1, 0, 1, 1, 1, 0, -1]
-dy = [0, 1, 1, 1, 0, -1, -1, -1]
+for i in range(n):
+    for j in range(n):
+        tree_map[i][j] = deque(sorted(tree_map[i][j]))
+dx = [-1,-1,-1,0,0,1,1,1]
+dy = [-1,0,1,-1,1,-1,0,1]
 
-# 봄, 여름 처리할 함수
 def spring_summer():
     for i in range(n):
         for j in range(n):
-            len_ = len(trees[i][j]) # 현재 위치에 있는 나무 총 개수
-            # 현재 위치의 나무들 탐색
-            for k in range(len_):
-                # 나무가 죽는 경우
-                if graph[i][j] < trees[i][j][k]:
-                    # 죽는 나무들은 따로 저장
-                    for _ in range(k, len_):
-                        dead_trees[i][j].append(trees[i][j].pop())
-                    break
-                # 나무가 양분 먹고 성장하는 경우
+            alive = deque()
+            for _ in range(len(tree_map[i][j])):
+                age = tree_map[i][j].popleft()
+                if land[i][j] >= age:
+                    land[i][j] -= age
+                    alive.append(age + 1)
                 else:
-                    graph[i][j] -= trees[i][j][k]
-                    trees[i][j][k] += 1
-    # 죽은 나무들 만큼 양분 저장
+                    dead[i][j].append(age)
+            tree_map[i][j] = alive
+
     for i in range(n):
         for j in range(n):
-            while dead_trees[i][j]:
-                graph[i][j] += dead_trees[i][j].pop() // 2
+            for age in dead[i][j]:
+                land[i][j] += age // 2
+            dead[i][j].clear() # 다음 년도를 위해 초기화
 
-# 가을, 겨울 처리할 함수
 def fall_winter():
     for i in range(n):
         for j in range(n):
-            # 현재 위치의 나무들 탐색
-            for k in range(len(trees[i][j])):
-                # 현재 나무의 나이가 씨를 뿌릴 수 있는 상태인 경우
-                if trees[i][j][k] % 5 == 0:
-                    # 8방향에 씨 뿌림
-                    for t in range(8):
-                        nx = i + dx[t]
-                        ny = j + dy[t]
-                        # 범위 체크
-                        if nx < 0 or nx >= n or ny < 0 or ny >= n:
-                            continue
-                        # 새로 태어난 나무들 앞으로 삽입
-                        trees[nx][ny].appendleft(1)
-            # 밭에 양분 추가
-            graph[i][j] += a[i][j]
+            for age in tree_map[i][j]:
+                if age % 5 == 0:
+                    for dir in range(8):
+                        nx = i + dx[dir]
+                        ny = j + dy[dir]
+                        if 0 <= nx < n and 0 <= ny < n:
+                            tree_map[nx][ny].appendleft(1)
+            land[i][j] += feed[i][j]
 
 
-for i in range(k):
+for year in range(k):
     spring_summer()
     fall_winter()
 
-answer = 0
+result = 0
 for i in range(n):
     for j in range(n):
-        answer += len(trees[i][j])
-
-print(answer)
+        result += len(tree_map[i][j])
+print(result)
