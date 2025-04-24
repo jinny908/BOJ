213169cf05496b4afc6dc8d6fa871678aa0daf26
Jinsoo Park <75584814+jinny908@users.noreply.github.com>
2025-04-24 21:35:24 +0900

    [level 2] Title: 중복 제거하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59408. 중복 제거하기/중복 제거하기.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+# SELECT count(distinct name) as 'count' from animal_ins
+# where name is not null;
+
+
+select count(distinct name) as 'count' from animal_ins
+where name is not null;
